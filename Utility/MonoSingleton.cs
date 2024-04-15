ce076d3d366c729c6b421b425df7e1eb9207107b
hoon <kjh1833000@naver.com>
2024-04-15 20:06:20 +0900

    [진훈] 모노 싱글톤 생성

---
+++ Utility/MonoSingleton.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace Utility
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance = null;
+        private static readonly object Lock = new object();
+        
+        public static T I
+        {
+            get
+            {
+                lock (Lock)
+                {
+                    if (instance != null)
+                    {
+                        return instance;
+                    }
+                
+                    var objs = FindObjectsOfType<T>();
+
+                    if (objs.Length > 0)
+                    {
+                        instance = objs[0];
+                    }
+
+                    if (instance != null)
+                    {
+                        return instance;
+                    }
+                    
+                    var objectName = typeof(T).ToString();
+                    var obj = GameObject.Find(objectName);
+                    
+                    if (obj == null)
+                    {
+                        obj = new GameObject(objectName);
+                    }
+                    
+                    instance = obj.AddComponent<T>();
+
+                    return instance;
+                }
+            }
+        }
+
+        public static void Clear()
+        {
+            if (instance == null)
+            {
+                return;
+            }
+        
+            DestroyImmediate(instance.gameObject);
+            instance = null;
+        }
+    }
+}
