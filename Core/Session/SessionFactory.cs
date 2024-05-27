b540259e4eee987abb05e5a52e5b0bcf2e7c937d
hoon <rlawlsgns2916@gmail.com>
2024-05-27 21:16:37 +0900

    [진훈] Session Factory 변수 이름 변경

--- Core/Session/SessionFactory.cs
+++ Core/Session/SessionFactory.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 
 public class SessionFactory : Singleton<SessionFactory>
 {
-    private readonly Dictionary<eSession, Func<ISession>> _factories = new Dictionary<eSession, Func<ISession>>();
+    private readonly Dictionary<eSession, Func<ISession>> factories = new Dictionary<eSession, Func<ISession>>();
         
     public ISession Create(eSession contentType)
     {
-        if (!_factories.TryGetValue(contentType, out var factory))
+        if (!factories.TryGetValue(contentType, out var factory))
         {
             throw new Exception($"No factory method set for {contentType}");
         }
@@ -18,6 +17,6 @@ public class SessionFactory : Singleton<SessionFactory>
 
     public void Register(eSession contentType, Func<ISession> factoryMethod)
     {
-        _factories[contentType] = factoryMethod;
+        factories[contentType] = factoryMethod;
     }
 }
